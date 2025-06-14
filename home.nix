882f5d28fd6b784e3d83d43d9869fb0edb86dc95
Jason Reslock <jreslock@users.noreply.github.com>
2025-06-14 11:38:45 -0400

    Fix home.nix

--- home.nix
+++ home.nix
@@ -1,16 +1,33 @@
-{ config, pkgs, ... }:
-
 {
-  home.stateVersion = "24.05";
+  description = "jreslock dotfiles with home-manager";
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    oh-my-zsh.enable = true;
-    shellAliases = {
-      ll = "ls -la";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  home.file.".zshrc".source = ./zsh/.zshrc;
+  outputs = { nixpkgs, home-manager, ... }: let
+    systems = [ "x86_64-darwin" "aarch64-darwin" "x86_64-linux" ];
+  in {
+    homeConfigurations = builtins.listToAttrs (map (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        name = system;
+        value = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          modules = [
+            {
+              home.username = "jreslock";
+              home.homeDirectory = "/Users/jreslock";
+            }
+            ./home.nix
+          ];
+        };
+      }) systems);
+  };
 }
